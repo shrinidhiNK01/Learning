4d2f472edb31485b2eba2abd8a75639541a97346
Shrinidhi N Kulkarni <shrinidhink0001@gmail.com>
2025-10-30 21:08:30 +0530

    I am learning

---
+++ snk.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class snk {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        int a=sc.nextInt();
+        System.out.println("Your input:" +a);
+    }
+}
